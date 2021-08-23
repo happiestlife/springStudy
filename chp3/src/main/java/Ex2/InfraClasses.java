92ed05d789f718c14f02275e883a082d401f30b1
chickenman <programmingStudy9999@gmail.com>
2021-08-23 22:27:28 +0900

    Revert "error corrected"
    This reverts commit d4625b8ea9d96f974354c5f90fbea65fb2ea8c79.

---
+++ chp3/src/main/java/Ex2/InfraClasses.java
@@ -0,0 +1,23 @@
+package Ex2;
+
+public class InfraClasses {
+    static void printFirstMenu(){
+        System.out.println("-----------------");
+        System.out.println("1. About customerInfo");
+        System.out.println("2. About printInfo");
+        System.out.print("-----------------\n>> ");
+    }
+    static void printAboutCustomerInfo(){
+        System.out.println("-----------------");
+        System.out.println("1. insert customer info");
+        System.out.println("2. modify customer info");
+        System.out.println("3. delete customer info");
+        System.out.print("-----------------\n>> ");
+    }
+    static void printAboutPrintInfo(){
+        System.out.println("-----------------");
+        System.out.println("1. print all customers info");
+        System.out.println("2. print particular customer info");
+        System.out.print("-----------------\n>> ");
+    }
+}
